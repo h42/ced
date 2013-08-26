d3f27039c26de5a29c3e718892fb153c0b60d0cc
h42 <jdinardo@nycap.rr.com>
2013-08-25 22:09:10 -0400

    ctrl_kd part 1

--- func2.cc
+++ func2.cc
@@ -11,10 +11,12 @@ void ced::ctrl_k() {
     int c = dsp.get();
     if (c==2  || c=='b' || c=='B') ctrl_kb();
     else if (c==3   || c=='c' || c=='C') ctrl_kc();
+    else if (c==4   || c=='d' || c=='D') ctrl_kd();
     else if (c==11  || c=='k' || c=='K') ctrl_kk();
     else if (c==12  || c=='l' || c=='L') ctrl_kl();
     else if (c==20  || c=='t' || c=='T')
         snprintf(zmsg,sizeof(zmsg),"in_k=%d",in_k());
+    else if (c==22   || c=='v' || c=='V') ctrl_kv();
 }
 
 int ced::check_k() {
@@ -41,13 +43,39 @@ void ced::ctrl_kb() {
     disp_k();
 }
 
-void ced::ctrl_kc() {
-    int l;
-    if (zkh<1 || (zkh==1 && in_k() && zx != zkx1) ) {
-        snprintf(zmsg,sizeof(zmsg),"nothing copied");
+void ced::ctrl_kd() {
+    if (zkh==0) {
+        snprintf(zmsg,sizeof(zmsg),"nothing deleted");
         return;
     }
     pline();
+    if (zkh==1) {
+        int i, l=zkx2-zkx1+1, oy=zy;
+        zy=zky1;
+        gline(1);
+        for (i=zkx1; i<zbufl-l; i++) zbuf[i]=zbuf[i+l];
+        memset(&zbuf[zbufl-l],' ',l);
+        zbufl-=l;
+        zkh=0;
+        if (zy==oy) {
+            if (zx>zkx2) zx-=l;
+            else if (zx>zkx1) zx = zkx1;
+        }
+        pline();
+        zy=oy;
+        disppage(ztop);
+        return;
+    }
+
+}
+
+void ced::ctrl_kv() {
+    int l;
+    pline();
+    if (zkh<1 || (zkh && in_k()) ) {
+        snprintf(zmsg,sizeof(zmsg),"nothing copied");
+        return;
+    }
     if (zkh==1) {
         gline2(zky1);
         l=zkx2-zkx1+1;
@@ -60,7 +88,24 @@ void ced::ctrl_kc() {
 	zbufl+=l;
         zkx1=zx; zkx2=zkx1+l-1; zky1=zky2=zy;
 	disppage(ztop);
+        return;
     }
+    list ll2;
+    int i;
+    char *buf;
+    zedit=1;
+    l=zky2-zky1+1;
+    ll2.init();
+    for (i=zky1;i<=zky2;i++) {
+        buf=ll.get(i);
+        ll2.ins(buf);
+    }
+    for (i=0;i<l;i++) {
+        buf=ll2.get(i);
+        ll.ins(i+zy+1,buf);
+    }
+    zky1=zy+1;zky2=zky1+l-1;
+    disppage(ztop);
 }
 
 void ced::ctrl_kk() {
@@ -78,6 +123,45 @@ void ced::ctrl_kl() {
     zky2=zy;
     if ((zkh=check_k())>0) disppage(ztop);
     disp_k();
+}
+
+void ced::ctrl_kc() {
+    int l;
+    pline();
+    if (zkh<1 || (zkh==1 && in_k() && zx != zkx1) || (zkh>1 && in_k() && zy != zky2)) {
+        snprintf(zmsg,sizeof(zmsg),"nothing copied");
+        return;
+    }
+    if (zkh==1) {
+        gline2(zky1);
+        l=zkx2-zkx1+1;
+        char *cp=(char *)alloca(l);
+        memcpy(cp,&zbuf2[zkx1],l);
+	gline(1);
+	memcpy(zbuf2,zbuf,zbufl);
+        memcpy(&zbuf[zx],cp,l);
+	memcpy(&zbuf[zx+l],&zbuf2[zx],zbufl-zx);
+	zbufl+=l;
+        zkx1=zx; zkx2=zkx1+l-1; zky1=zky2=zy;
+	disppage(ztop);
+        return;
+    }
+    list ll2;
+    int i;
+    char *buf;
+    zedit=1;
+    l=zky2-zky1+1;
+    ll2.init();
+    for (i=zky1;i<=zky2;i++) {
+        buf=ll.get(i);
+        ll2.ins(buf);
+    }
+    for (i=0;i<l;i++) {
+        buf=ll2.get(i);
+        ll.ins(i+zy+1,buf);
+    }
+    zky1=zy+1;zky2=zky1+l-1;
+    disppage(ztop);
 }
 
 void ced::disp_k() {
